5f0b629c23d9baf6e13ded4ba2344cea295d568e
code-by-neha <menehalalwani@gmail.com>
2025-07-01 19:39:13 +0530

    all checkbox don

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <>
-    <div className="passwordBox">
-      <h2>Password Generator</h2>
-      
-      <div className='passwordBoxIn'>
-        <input type='text' />
-        <button>Copy</button>
-      </div>
+      <div className="passwordBox">
+        <h2>Password Generator</h2>
 
-      <div className="passLength">
-        <label>Password Length</label>
-        <input type='number'/>
+        <div className="passwordBoxIn">
+          <input type="text" />
+          <button>Copy</button>
+        </div>
+
+        <div className="passLength">
+          <label>Password Length</label>
+          <input type="number" max={20} />
+        </div>
+
+        <div className="passLength">
+          <label>Include UpperCase Letters</label>
+          <input type="checkbox" />
+        </div>
+
+        <div className="passLength">
+          <label>Include LowerCase Letters</label>
+          <input type="checkbox" />
+        </div>
+
+        <div className="passLength">
+          <label>Include Numbers</label>
+          <input type="checkbox" />
+        </div>
+
+        <div className="passLength">
+          <label>Include Symbols</label>
+          <input type="checkbox" />
+        </div>
       </div>
-    </div>
     </>
   );
 }
