b7f005162f2312ac0177d2a53c2e5936f998ed79
code-by-neha <menehalalwani@gmail.com>
2025-07-02 17:35:03 +0530

    All done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import { useState } from "react";
 import "./App.css";
+import {LC,UC,NC,SC} from "./data/PassChar"
 
 function App() {
 let [uppercase,setUppercase]=useState(false);
 let [lowercase,setLowercase]=useState(false);
 let [number,setNumber]=useState(false);
 let [symbols,setSymbols]=useState(false);
+let [passwordlen,setPasswordlen]=useState(10);
+let [fpass,setPass]=useState('');
 
 let createPassword = () => {
   let charSet=''
   if(uppercase || lowercase || number || symbols)
   {
+    let finalpass=''
     if(uppercase) charSet+=UC;
     if(lowercase) charSet+=LC;
     if(number) charSet+=NC;
     if(symbols) charSet+=SC;
+    for(let i=0;i<passwordlen;i++)
+    {
+      finalpass+=charSet.charAt(Math.floor(Math.random()*charSet.length))
+    }
+    setPass(finalpass);
   }
   else{
     alert("Select CheckBox !")
@@ -27,13 +36,13 @@ let createPassword = () => {
         <h2>Password Generator</h2>
 
         <div className="passwordBoxIn">
-          <input type="text" />
+          <input type="text" value={fpass} readOnly />
           <button>Copy</button>
         </div>
 
         <div className="passLength">
           <label>Password Length</label>
-          <input type="number" max={20} min={1} />
+          <input type="number" max={20} min={4} value={passwordlen} onChange={(event)=>setPasswordlen(event.target.value)}/>
         </div>
 
         <div className="passLength">
