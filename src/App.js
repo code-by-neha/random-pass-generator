e686bc22a33edcab4dd972befee3ff6076f56194
code-by-neha <menehalalwani@gmail.com>
2025-07-02 17:41:44 +0530

    copy button done

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
 import { useState } from "react";
 import "./App.css";
-import {LC,UC,NC,SC} from "./data/PassChar"
+import { LC, UC, NC, SC } from "./data/PassChar";
 
 function App() {
-let [uppercase,setUppercase]=useState(false);
-let [lowercase,setLowercase]=useState(false);
-let [number,setNumber]=useState(false);
-let [symbols,setSymbols]=useState(false);
-let [passwordlen,setPasswordlen]=useState(10);
-let [fpass,setPass]=useState('');
+  let [uppercase, setUppercase] = useState(false);
+  let [lowercase, setLowercase] = useState(false);
+  let [number, setNumber] = useState(false);
+  let [symbols, setSymbols] = useState(false);
+  let [passwordlen, setPasswordlen] = useState(10);
+  let [fpass, setPass] = useState("");
 
-let createPassword = () => {
-  let charSet=''
-  if(uppercase || lowercase || number || symbols)
-  {
-    let finalpass=''
-    if(uppercase) charSet+=UC;
-    if(lowercase) charSet+=LC;
-    if(number) charSet+=NC;
-    if(symbols) charSet+=SC;
-    for(let i=0;i<passwordlen;i++)
-    {
-      finalpass+=charSet.charAt(Math.floor(Math.random()*charSet.length))
+  let createPassword = () => {
+    let charSet = "";
+    if (uppercase || lowercase || number || symbols) {
+      let finalpass = "";
+      if (uppercase) charSet += UC;
+      if (lowercase) charSet += LC;
+      if (number) charSet += NC;
+      if (symbols) charSet += SC;
+      for (let i = 0; i < passwordlen; i++) {
+        finalpass += charSet.charAt(Math.floor(Math.random() * charSet.length));
+      }
+      setPass(finalpass);
+    } else {
+      alert("Select CheckBox !");
     }
-    setPass(finalpass);
-  }
-  else{
-    alert("Select CheckBox !")
-  }
-}
+  };
 
   return (
     <>
@@ -37,36 +34,66 @@ let createPassword = () => {
 
         <div className="passwordBoxIn">
           <input type="text" value={fpass} readOnly />
-          <button>Copy</button>
+          <button
+            onClick={() => {
+              navigator.clipboard.writeText(fpass);
+              alert("Password copied!");
+            }}
+          >
+            Copy
+          </button>
         </div>
 
         <div className="passLength">
           <label>Password Length</label>
-          <input type="number" max={20} min={4} value={passwordlen} onChange={(event)=>setPasswordlen(event.target.value)}/>
+          <input
+            type="number"
+            max={20}
+            min={4}
+            value={passwordlen}
+            onChange={(event) => setPasswordlen(event.target.value)}
+          />
         </div>
 
         <div className="passLength">
           <label>Include UpperCase Letters</label>
-          <input type="checkbox" checked={uppercase} onChange={()=>setUppercase(!uppercase)} />
+          <input
+            type="checkbox"
+            checked={uppercase}
+            onChange={() => setUppercase(!uppercase)}
+          />
         </div>
 
         <div className="passLength">
           <label>Include LowerCase Letters</label>
-          <input type="checkbox" checked={lowercase} onChange={()=>setLowercase(!lowercase)} />
+          <input
+            type="checkbox"
+            checked={lowercase}
+            onChange={() => setLowercase(!lowercase)}
+          />
         </div>
 
         <div className="passLength">
           <label>Include Numbers</label>
-          <input type="checkbox" checked={number} onChange={()=>setNumber(!number)} />
+          <input
+            type="checkbox"
+            checked={number}
+            onChange={() => setNumber(!number)}
+          />
         </div>
 
         <div className="passLength">
           <label>Include Symbols</label>
-          <input type="checkbox" checked={symbols} onChange={()=>setSymbols(!symbols)} />
+          <input
+            type="checkbox"
+            checked={symbols}
+            onChange={() => setSymbols(!symbols)}
+          />
         </div>
-      
-          <button className="btn" onClick={createPassword}>Generate Password</button>
-    
+
+        <button className="btn" onClick={createPassword}>
+          Generate Password
+        </button>
       </div>
     </>
   );
