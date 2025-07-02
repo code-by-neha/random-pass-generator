36a5ac3cbccf493edfe95bb2e6a32bb605db5597
code-by-neha <menehalalwani@gmail.com>
2025-07-02 16:31:12 +0530

    All states done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-let [uppercase,setuppercase]=useState(false);
+let [uppercase,setUppercase]=useState(false);
+let [lowercase,setLowercase]=useState(false);
+let [number,setNumber]=useState(false);
+let [symbols,setSymbols]=useState(false);
 
   return (
     <>
@@ -21,22 +24,22 @@ let [uppercase,setuppercase]=useState(false);
 
         <div className="passLength">
           <label>Include UpperCase Letters</label>
-          <input type="checkbox" checked={uppercase} onChange={()=>setuppercase(!uppercase)} />
+          <input type="checkbox" checked={uppercase} onChange={()=>setUppercase(!uppercase)} />
         </div>
 
         <div className="passLength">
           <label>Include LowerCase Letters</label>
-          <input type="checkbox" />
+          <input type="checkbox" checked={lowercase} onChange={()=>setLowercase(!lowercase)} />
         </div>
 
         <div className="passLength">
           <label>Include Numbers</label>
-          <input type="checkbox" />
+          <input type="checkbox" checked={number} onChange={()=>setNumber(!number)} />
         </div>
 
         <div className="passLength">
           <label>Include Symbols</label>
-          <input type="checkbox" />
+          <input type="checkbox" checked={symbols} onChange={()=>setSymbols(!symbols)} />
         </div>
 
         <button>Generate Password</button>
